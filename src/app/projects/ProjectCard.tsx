dd85df9ab131a54dae4ddfacb1cfa7e2f728f694
Alberto Paim <albertoopaim@gmail.com>
2025-08-07 21:51:17 -0300

    fix: projectCard opacity

--- src/app/projects/ProjectCard.tsx
+++ src/app/projects/ProjectCard.tsx
@@ -1,48 +1,41 @@
 import Image from "next/image";
 import Link from "next/link";
-import { MdCode, MdLink } from "react-icons/md";
+import { MdCode, MdLink, MdWeb } from "react-icons/md";
 
 interface ProjectCardProps {
     name: string;
     description: string;
     image: string;
     webLink: string;
-    codeLink: string;
 }
 
-export const ProjecCard = ({ name, description, image, webLink, codeLink }: ProjectCardProps) => {
+export const ProjecCard = ({ name, description, image, webLink }: ProjectCardProps) => {
     return (
         <div className="text-white">
 
             <div className="relative rounded-t-xl group ">
 
                 <Image
-                    className="rounded-t-2xl mx-auto min-w-full min-h-96 opacity-40 transition-opacity duration-300 group-hover:opacity-70"
+                    className="rounded-t-2xl mx-auto min-w-full min-h-96 opacity-30 md:opacity-80 transition-opacity duration-300 group-hover:opacity-30"
                     src={image}
                     width={1000}
                     height={400}
                     alt="project image"
                 />
 
-                <div className="absolute inset-0 flex items-center justify-center gap-4 text-3xl text-zinc-400 md:opacity-0 opacity-100 md:group-hover:opacity-100 transition-opacity duration-300">
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 inset-0 flex flex-col items-center justify-center gap-4 text-5xl text-zinc-400 md:opacity-0 opacity-100 md:group-hover:opacity-100 transition-opacity duration-300">
                     <Link
-                        className="border border-white rounded-full p-2 hover:text-white "
+                        className="border border-white rounded-full p-4 hover:text-white "
                         href={webLink}
-                        target="_blank"
-                    >
-                        <MdLink />
-                    </Link>
-
-                    <Link
-                        className="border p-2 border-white rounded-full  hover:text-white"
-                        href={codeLink}
                         target="_blank"
                     >
                         <MdCode />
                     </Link>
+
+                    <span className="text-3xl text-white">Visitar Projeto</span>
                 </div>
-                <h3 className="text-center md:text-start  text-xl px-2 pt-2 group-hover:text-acao">{name}</h3>
-                <p className="text-center md:text-start  text-zinc-400 px-2 pb-2 group-hover:text-white">{description}</p>
+                <h3 className="text-2xl text-center md:text-start  font-bold px-2 pt-2 group-hover:text-acao">{name}</h3>
+                <p className="text-xl text-center md:text-start  text-zinc-400 px-2 pb-2 group-hover:text-white">{description}</p>
             </div>
 
 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,33 +4,32 @@ export const Projects = () => {
 
     const projects = [
         {
-            name: "Portfolio",
-            description: "Portifolio feito com Next.js, TailwindCss e Typescript",
-            image: "/project1.png",
-            webLink: "/",
-            codeLink: "/"
+            name: "Smart Election",
+            description: "Este projeto implementa um contrato inteligente de eleição descentralizada utilizando Solidity. Permite que um administrador cadastre candidatos, inicie e encerre a votação, e que usuários votem de forma segura (somente uma vez). Feito usando Solidity e Hardhat",
+            image: "/project4.png",
+            webLink: "https://github.com/AlbertoPaim/Election-System"
         },
+
         {
             name: "Lista de Mercado - Api",
             description: "A Lista de Mercado é uma API RESTful para gerenciar compras e itens de compras, com operações para criar, ler, atualizar e excluir tanto compras quanto itens. Feito usando Java, Spring Boot e PostgresSQL",
             image: "/project2.png",
-            webLink: "https://github.com/AlbertoPaim/Lista-de-mercado",
-            codeLink: "https://github.com/AlbertoPaim/Lista-de-mercado"
+            webLink: "https://github.com/AlbertoPaim/Lista-de-mercado"
         },
         {
             name: "Ciclos",
             description: "Site para mulheres gerenciarem seus ciclos mentruais com calculadora da próxima menstruação, feito com Html, Css e javascript",
             image: "/project3.png",
             webLink: "https://albertopaim.github.io/Ciclos/",
-            codeLink: "https://github.com/AlbertoPaim/Ciclos"
         },
         {
+
             name: "Portfolio",
-            description: "Este projeto implementa um contrato inteligente de eleição descentralizada utilizando Solidity. Permite que um administrador cadastre candidatos, inicie e encerre a votação, e que usuários votem de forma segura (somente uma vez). Feito usando Solidity e Hardhat",
-            image: "/project4.png",
-            webLink: "https://github.com/AlbertoPaim/Election-System",
-            codeLink: "https://github.com/AlbertoPaim/Election-System"
-        }
+            description: "Portifolio feito com Next.js, TailwindCss e Typescript",
+            image: "/project1.png",
+            webLink: "/"
+        },
+
     ]
 
 
@@ -40,7 +39,7 @@ export const Projects = () => {
         <section id="projects" className="flex flex-col pt-18  overflow-hidden px-4 gap-10 justify-center mx-auto max-w-[1080px] ">
             <h1 className="text-4xl lg:text-6xl text-white uppercase font-black text-center drop-shadow-[0_0_3px_#fff]" >Projects</h1>
             <div>
-                <ul className="grid md:grid-cols-2 gap-4">
+                <ul className="grid md:grid-cols-2 md:gap-10 lg:gap-20 px-4">
                     {projects.map((item, index) => (
                         <li
                             className="rounded-t-2xl bg-black"
@@ -50,7 +49,6 @@ export const Projects = () => {
                                 description={item.description}
                                 image={item.image}
                                 webLink={item.webLink}
-                                codeLink={item.codeLink}
                             />
                         </li>
                     ))}
